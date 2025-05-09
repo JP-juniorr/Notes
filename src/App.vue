f37dce414e687f220c7325b90268758ee589aae9
JP-juniorr <jp.noname@icloud.com>
2025-05-09 12:39:30 +0200

    Html Css done

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,92 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup></script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button>+</button>
+      </header>
+      <div class="cards-container">
+        <div class="card">
+          <p class="main-text">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil,
+            iure impedit fuga obcaecati neque enim commodi est tempore molestias
+            voluptatibus blanditiis atque nostrum quis nobis ex aperiam suscipit
+            corporis repellat eos animi
+          </p>
+          <p class="date">2023-10-01 12:00</p>
+        </div>
+        <div class="card">
+          <p class="main-text">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil,
+            iure impedit fuga obcaecati neque enim commodi est tempore molestias
+            voluptatibus blanditiis atque nostrum quis nobis ex aperiam suscipit
+            corporis repellat eos animi
+          </p>
+          <p class="date">2023-10-01 12:00</p>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+main {
+  height: 100vh;
+  width: 100vw;
+  font-family: "Poppins", sans-serif;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.container {
+  max-width: 1000px;
+  padding: 10px;
+  margin: 0 auto;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+}
+h1 {
+  font-size: 75px;
+  font-weight: bold;
+  margin-bottom: 25px;
+  color: #333;
+}
+button {
+  background-color: rgb(21, 20, 20);
+  border: none;
+  padding: 10px;
+  font-size: 20px;
+  height: 50px;
+  width: 50px;
+  cursor: pointer;
+  border-radius: 100%;
+  color: white;
+  font-size: 20px;
+}
+.card {
+  width: 250px;
+  height: 250px;
+  background: #cb9a9a;
+  padding: 10px;
+  border-radius: 15px;
+  font-size: 14px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-right: 20px;
+  margin-bottom: 20px;
+}
+.date {
+  font-size: 12px;
+  color: #fff;
+  text-align: right;
+}
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
 }
 </style>
